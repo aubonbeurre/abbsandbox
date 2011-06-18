7cd5f986e3c12299b3823eedb21e5c5bd03e19f9
Alexandre Parenteau <aubonbeurre@gmail.com>
2011-06-18 09:48:38 +0200

    format

--- thrift_async_test/CppClient.cpp
+++ thrift_async_test/CppClient.cpp
@@ -38,49 +38,49 @@ using namespace shared;
 using namespace boost;
 
 int main(int argc, char** argv) {
-  shared_ptr<TTransport> socket(new TSocket("localhost", 9090));
-  //shared_ptr<TTransport> transport(new TBufferedTransport(socket));
-  shared_ptr<TTransport> transport(new TFramedTransport(socket));
-  shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
+	shared_ptr<TTransport> socket(new TSocket("localhost", 9090));
+	//shared_ptr<TTransport> transport(new TBufferedTransport(socket));
+	shared_ptr<TTransport> transport(new TFramedTransport(socket));
+	shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
 
-  CalculatorClient client(protocol);
+	CalculatorClient client(protocol);
 
-  try {
-    transport->open();
+	try {
+		transport->open();
 
-    client.ping();
-    printf("ping()\n");
+		client.ping();
+		printf("ping()\n");
 
-    int32_t sum = client.add(1,1);
-    printf("1+1=%d\n", sum);
+		int32_t sum = client.add(1, 1);
+		printf("1+1=%d\n", sum);
 
-    Work work;
-    work.op = Operation::DIVIDE;
-    work.num1 = 1;
-    work.num2 = 0;
+		Work work;
+		work.op = Operation::DIVIDE;
+		work.num1 = 1;
+		work.num2 = 0;
 
-    try {
-      int32_t quotient = client.calculate(1, work);
-      printf("Whoa? We can divide by zero!\n");
-    } catch (InvalidOperation &io) {
-      printf("InvalidOperation: %s\n", io.why.c_str());
-    }
+		try {
+			int32_t quotient = client.calculate(1, work);
+			printf("Whoa? We can divide by zero!\n");
+		} catch (InvalidOperation &io) {
+			printf("InvalidOperation: %s\n", io.why.c_str());
+		}
 
-    work.op = Operation::SUBTRACT;
-    work.num1 = 15;
-    work.num2 = 10;
-    int32_t diff = client.calculate(1, work);
-    printf("15-10=%d\n", diff);
+		work.op = Operation::SUBTRACT;
+		work.num1 = 15;
+		work.num2 = 10;
+		int32_t diff = client.calculate(1, work);
+		printf("15-10=%d\n", diff);
 
-    // Note that C++ uses return by reference for complex types to avoid
-    // costly copy construction
-    SharedStruct ss;
-    client.getStruct(ss, 1);
-    printf("Check log: %s\n", ss.value.c_str());
+		// Note that C++ uses return by reference for complex types to avoid
+		// costly copy construction
+		SharedStruct ss;
+		client.getStruct(ss, 1);
+		printf("Check log: %s\n", ss.value.c_str());
 
-    transport->close();
-  } catch (TException &tx) {
-    printf("ERROR: %s\n", tx.what());
-  }
+		transport->close();
+	} catch (TException &tx) {
+		printf("ERROR: %s\n", tx.what());
+	}
 
 }
--- thrift_async_test/CppServer.cpp
+++ thrift_async_test/CppServer.cpp
@@ -45,122 +45,125 @@ using namespace shared;
 
 using namespace boost;
 
-class CalculatorHandler : public CalculatorIf {
- public:
-  CalculatorHandler() {}
+class CalculatorHandler: public CalculatorIf {
+public:
+	CalculatorHandler() {
+	}
 
-  void ping() {
-    printf("ping()\n");
-  }
+	void ping() {
+		printf("ping()\n");
+	}
 
-  int32_t add(const int32_t n1, const int32_t n2) {
-    printf("add(%d,%d)\n", n1, n2);
-    return n1 + n2;
-  }
+	int32_t add(const int32_t n1, const int32_t n2) {
+		printf("add(%d,%d)\n", n1, n2);
+		return n1 + n2;
+	}
 
-  int32_t calculate(const int32_t logid, const Work &work) {
-    printf("calculate(%d,{%d,%d,%d})\n", logid, work.op, work.num1, work.num2);
-    int32_t val;
+	int32_t calculate(const int32_t logid, const Work &work) {
+		printf("calculate(%d,{%d,%d,%d})\n", logid, work.op, work.num1,
+				work.num2);
+		int32_t val;
 
-    switch (work.op) {
-    case Operation::ADD:
-      val = work.num1 + work.num2;
-      break;
-    case Operation::SUBTRACT:
-      val = work.num1 - work.num2;
-      break;
-    case Operation::MULTIPLY:
-      val = work.num1 * work.num2;
-      break;
-    case Operation::DIVIDE:
-      if (work.num2 == 0) {
-        InvalidOperation io;
-        io.what = work.op;
-        io.why = "Cannot divide by 0";
-        throw io;
-      }
-      val = work.num1 / work.num2;
-      break;
-    default:
-      InvalidOperation io;
-      io.what = work.op;
-      io.why = "Invalid Operation";
-      throw io;
-    }
+		switch (work.op) {
+		case Operation::ADD:
+			val = work.num1 + work.num2;
+			break;
+		case Operation::SUBTRACT:
+			val = work.num1 - work.num2;
+			break;
+		case Operation::MULTIPLY:
+			val = work.num1 * work.num2;
+			break;
+		case Operation::DIVIDE:
+			if (work.num2 == 0) {
+				InvalidOperation io;
+				io.what = work.op;
+				io.why = "Cannot divide by 0";
+				throw io;
+			}
+			val = work.num1 / work.num2;
+			break;
+		default:
+			InvalidOperation io;
+			io.what = work.op;
+			io.why = "Invalid Operation";
+			throw io;
+		}
 
-    SharedStruct ss;
-    ss.key = logid;
-    char buffer[12];
-    snprintf(buffer, sizeof(buffer), "%d", val);
-    ss.value = buffer;
+		SharedStruct ss;
+		ss.key = logid;
+		char buffer[12];
+		snprintf(buffer, sizeof(buffer), "%d", val);
+		ss.value = buffer;
 
-    log[logid] = ss;
+		log[logid] = ss;
 
-    return val;
-  }
+		return val;
+	}
 
-  void getStruct(SharedStruct &ret, const int32_t logid) {
-    printf("getStruct(%d)\n", logid);
-    ret = log[logid];
-  }
+	void getStruct(SharedStruct &ret, const int32_t logid) {
+		printf("getStruct(%d)\n", logid);
+		ret = log[logid];
+	}
 
-  void zip() {
-    printf("zip()\n");
-  }
+	void zip() {
+		printf("zip()\n");
+	}
 
 protected:
-  map<int32_t, SharedStruct> log;
+	map<int32_t, SharedStruct> log;
 
 };
 
 int main(int argc, char **argv) {
 
-  shared_ptr<TProtocolFactory> protocolFactory(new TBinaryProtocolFactory());
-  shared_ptr<CalculatorHandler> handler(new CalculatorHandler());
-  shared_ptr<TProcessor> processor(new CalculatorProcessor(handler));
+	shared_ptr<TProtocolFactory> protocolFactory(new TBinaryProtocolFactory());
+	shared_ptr<CalculatorHandler> handler(new CalculatorHandler());
+	shared_ptr<TProcessor> processor(new CalculatorProcessor(handler));
 
-  // using thread pool with maximum 15 threads to handle incoming requests
-  shared_ptr<ThreadManager> threadManager = ThreadManager::newSimpleThreadManager(15);
-  shared_ptr<PosixThreadFactory> threadFactory = shared_ptr<PosixThreadFactory>(new PosixThreadFactory());
-  threadManager->threadFactory(threadFactory);
-  threadManager->start();
+	// using thread pool with maximum 15 threads to handle incoming requests
+	shared_ptr<ThreadManager> threadManager =
+			ThreadManager::newSimpleThreadManager(15);
+	shared_ptr<PosixThreadFactory> threadFactory = shared_ptr<
+			PosixThreadFactory> (new PosixThreadFactory());
+	threadManager->threadFactory(threadFactory);
+	threadManager->start();
 
-  TNonblockingServer server(processor, protocolFactory, 9090, threadManager);
+	TNonblockingServer server(processor, protocolFactory, 9090, threadManager);
 
-  /*  shared_ptr<TServerTransport> serverTransport(new TServerSocket(9090));
-  shared_ptr<TTransportFactory> transportFactory(new TBufferedTransportFactory());
+	/*  shared_ptr<TServerTransport> serverTransport(new TServerSocket(9090));
+	 shared_ptr<TTransportFactory> transportFactory(new TBufferedTransportFactory());
 
-  TSimpleServer server(processor,
-                       serverTransport,
-                       transportFactory,
-                       protocolFactory);
-  */
+	 TSimpleServer server(processor,
+	 serverTransport,
+	 transportFactory,
+	 protocolFactory);
+	 */
 
+	/**
+	 * Or you could do one of these
 
-  /**
-   * Or you could do one of these
+	 shared_ptr<ThreadManager> threadManager =
+	 ThreadManager::newSimpleThreadManager(workerCount);
+	 shared_ptr<PosixThreadFactory> threadFactory =
+	 shared_ptr<PosixThreadFactory>(new PosixThreadFactory());
+	 threadManager->threadFactory(threadFactory);
+	 threadManager->start();
+	 TThreadPoolServer server(processor,
+	 serverTransport,
+	 transportFactory,
+	 protocolFactory,
+	 threadManager);
 
-  shared_ptr<ThreadManager> threadManager =
-    ThreadManager::newSimpleThreadManager(workerCount);
-  shared_ptr<PosixThreadFactory> threadFactory =
-    shared_ptr<PosixThreadFactory>(new PosixThreadFactory());
-  threadManager->threadFactory(threadFactory);
-  threadManager->start();
-  TThreadPoolServer server(processor,
-                           serverTransport,
-                           transportFactory,
-                           protocolFactory,
-                           threadManager);
+	 TThreadedServer server(processor,
+	 serverTransport,
+	 transportFactory,
+	 protocolFactory);
 
-  TThreadedServer server(processor,
-                         serverTransport,
-                         transportFactory,
-                         protocolFactory);
+	 */
 
-  */
-
-  printf("Starting the server...\n");
-  server.serve();
-  printf("done.\n");
-  return 0;
+	printf("Starting the server...\n");
+	server.serve();
+	printf("done.\n");
+	return 0;
 }
