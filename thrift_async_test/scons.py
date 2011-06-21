6bd5164272b77a5fab0e05ac3152d82855e1a7db
Alexandre Parenteau <aubonbeurre@gmail.com>
2011-06-21 07:55:24 -0700

    fix vcproj

---
+++ thrift_async_test/scons.py
@@ -0,0 +1,11 @@
+import os.path
+import sys
+
+thisfile = os.path.abspath(os.path.normpath(__file__))
+pythonlib = os.path.join(os.path.dirname(os.path.dirname(thisfile)), "pythonlib")
+sys.path.insert(0, pythonlib)
+
+import SCons.Script
+
+sys.argv += ("--no-cache", "-C", os.path.dirname(thisfile))
+SCons.Script.main()
