4b56ae8fb87b24f893bb96dc722328e290cd2c91
Alexandre Parenteau <aubonbeurre@gmail.com>
2011-06-24 08:42:36 -0700

    java test

---
+++ java_test/src/com/abb/mtest/MTest.java
@@ -0,0 +1,13 @@
+
+package com.abb.mtest;
+
+import org.apache.thrift.async.TAsyncClient;
+
+class MTest {
+   public static void main (String[] args){
+   	System.out.println("Hello World");
+   	
+   	//new TAsyncClient()
+   }
+}
+
